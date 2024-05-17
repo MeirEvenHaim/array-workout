c56a7f5e61916677e89377bfa0c0ef45928cd700
MeirEvenHaim <meirevenhaim018@gmail.com>
2024-05-17 13:01:46 +0300

    first commit

---
+++ project_1.py
@@ -0,0 +1,32 @@
+#make a pyramid using the for loop the loop iterates through the numbers 1-6 
+# pyramid = ""
+# for i in range(1,6):
+#     x = i
+#     i = str(i)
+#     i*=x
+#     pyramid = i
+#     print(pyramid)
+
+
+# for i in range(1,10):
+#     print(str(i) * i)
+# def arab_lover():
+#     for i in range(10,0,-1):
+#         print(i)
+        
+# arab_lover()
+
+
+lists =[3,8,2,7,4]
+target = 7
+x = 0
+while x  < len(lists):
+    # print(lists[x])
+    if lists[x] == target:
+        print(x)
+
+        
+    
+    
+    x+=1
+    
